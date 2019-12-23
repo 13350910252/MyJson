fe13428f8d3fa8136ab068eb490e90dcbfe9c84e
tyler <617606032@qq.com>
2019-12-23 17:23:29 +0800

    test

--- app/src/main/java/com/example/myjson/MainActivity.java
+++ app/src/main/java/com/example/myjson/MainActivity.java
@@ -9,28 +9,35 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class MainActivity extends AppCompatActivity {
+    public static final String json = "{\"message\":{\"{\"one\":\"one\",\"two\":\"two\"}\":\"{\"one\":\"one\",\"two\":\"two\"}\"}}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         set();
-        Log.d("abcd", "onCreate: ");
+        get();
+    }
+
+    private void get() {
+        JSONObject jsonObject = new JSONObject();
+
     }
 
     private void set() {
         try {
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("one","one");
-            jsonObject.put("two","two");
+            jsonObject.put("one", "one");
+            jsonObject.put("two", "two");
             JSONArray jsonArray = new JSONArray();
             jsonArray.put(jsonObject.toString());
 
             JSONObject jsonObject1 = new JSONObject();
-            jsonObject1.put("message",jsonArray.toJSONObject(jsonArray));//转换的string中\少
+            jsonObject1.put("message", jsonArray.toJSONObject(jsonArray));//转换的string中\少
 //            jsonObject1.put("root",jsonArray.toString());//转换的string中\多
-            Log.d("abcd", "set333: "+jsonObject1.toString());
+            Log.d("abcd", "set333: " + jsonObject1.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
